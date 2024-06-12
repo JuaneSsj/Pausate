be7a41f984136120b25337e02254579bb1290fab
GUADA <GUADA@192.168.1.55>
2024-06-11 22:31:02 -0500

    historial administrador

--- src/interfaz/HistorialAdm.java
+++ src/interfaz/HistorialAdm.java
@@ -155,6 +155,7 @@ public class HistorialAdm extends javax.swing.JFrame {
         Principal principal = new Principal();
         principal.setVisible(true);
         this.dispose();
+        
     }//GEN-LAST:event_botonCerrarSesionHistorialAdmActionPerformed
 
     /**
